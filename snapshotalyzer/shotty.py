85bfec454d9ce1d1a32633ce8fc523a1d2f19313
Chris Pierce <chrisjpierce@gmail.com>
2019-04-12 11:00:21 +0000

    add Click, flesh out list instances function

--- snapshotalyzer/shotty.py
+++ snapshotalyzer/shotty.py
@@ -1,8 +1,31 @@
+
 import boto3
+import sys
+import click
+
+@click.command()
+
+def list_instances():
+    "List EC2 Instances"
+    for i in ec2.instances.all():
+        print(", ".join(
+                            (
+                            i.id, 
+                            i.instance_type,
+                            i.placement["AvailabilityZone"],
+                            i.state['Name'],
+                            i.public_dns_name
+                            )
+                        )
+        )
 
 if __name__ == '__main__':
+    print(sys.argv)
     
+    # setup
     session = boto3.Session(profile_name="snapshotalyzer")
     ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    
+    # script
+    list_instances()
+    
